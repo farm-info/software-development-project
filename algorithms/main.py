d3a68af5ffa33a4b7812bc6ccacc907f31290c96
meow-d <isaacwenyi@gmail.com>
2024-01-24 14:40:58 +0800

    add search mode feature

--- algorithms/main.py
+++ algorithms/main.py
@@ -3,13 +3,16 @@ import warnings
 import tfidf
 
 
+# change this to switch between search and recommendation mode
+search_mode = False
 warnings.filterwarnings("ignore")
 
 
-print("Loading dataset... ", end="")
 # put your datasets in /algorithms/datasets/ so that git will ignore them
+print("Loading dataset... ", end="")
 dataset = tfidf.load_dataset("algorithms/datasets/recipe_nlg_lite/train.csv", "	")
 print("done")
+
 print("Processing dataset... ", end="")
 similarity, indices = tfidf.process_data(dataset, use_alt_stopwords=True)
 print("done")
@@ -19,26 +22,44 @@ print(
     dedent(
         """
         Please Enter the uid of the recipe you want to find similar recipes for.
+        Enter 'exit' to exit the program.
+        Enter 'switch' to switch between search or recommendation mode.
         You can also view the current recommendations by opening the csv file.
-        Enter 'exit' to exit the program"""
+        """
     )
 )
 
+
 while True:
-    query = input("\nEnter uid: ")
+    query = input("\nEnter query: ")
+
     if query == "exit":
         break
 
-    try:
-        recommendations = tfidf.get_recommendations(query, dataset, similarity, indices)
-    except ValueError:
-        print("Invalid uid")
-        continue
+    if search_mode:
+        try:
+            recommendations = tfidf.search_recipes(query, dataset, similarity)
+        except ValueError:
+            print("Invalid uid")
+            continue
 
-    recommendations.style.hide(axis='index')
-    print(
-        f"Recommendations for {dataset.loc[dataset["uid"] == query]["name"].values[0]}"
-    )
+        recommendations.style.hide(axis='index')
+        print(
+            f"Search results for {query}"
+        )
+
+    else:
+        try:
+            recommendations = tfidf.get_recommendations(query, dataset, similarity, indices)
+        except ValueError:
+            print("Invalid uid")
+            continue
+
+        recommendations.style.hide(axis='index')
+        print(
+            f"Recommendations for {dataset.loc[dataset["uid"] == query]["name"].values[0]}"
+        )
+
     print(f"{recommendations[["name","description", "uid"]]}")
     print("----------------------------------")
     recommendations.to_csv("algorithms/results/current.csv")

602dc57b8cecffa2e4e0b4ffab0e25a7dc374862
meow-d <isaacwenyi@gmail.com>
2024-02-01 00:02:19 +0800

    basic register and login functionality

--- recipeapp/accounts/views.py
+++ recipeapp/accounts/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect(
+                "home"
+            )  # 'home' should be replaced with the name of your home URL pattern
+    else:
+        form = AuthenticationForm()
+    return render(request, "login.html", {"form": form})
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect("success")
+            # For example, you can redirect the user to a success page
+    else:
+        form = UserCreationForm()
+    return render(request, "register.html", {"form": form})
+
+def success(request):
+    return render(request, "success.html")

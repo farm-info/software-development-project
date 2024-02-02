a22415a882147b5a7fc819affb52071ca4d90f08
meow-d <isaacwenyi@gmail.com>
2024-02-02 20:34:13 +0800

    code style

--- recipeapp/recipeapp/urls.py
+++ recipeapp/recipeapp/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("recipes.urls")),
-    path('accounts/', include('accounts.urls')),
+    path("accounts/", include("accounts.urls")),
     # path('accounts/', include('django.contrib.auth.urls')),
     path("admin/", admin.site.urls),
 ]

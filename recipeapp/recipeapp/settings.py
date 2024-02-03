69097d980cb91019050f3e194dd76ae84c971d23
meow-d <isaacwenyi@gmail.com>
2024-02-03 23:56:19 +0800

    fix media root path

--- recipeapp/recipeapp/settings.py
+++ recipeapp/recipeapp/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Media files (Uploaded by users)
 MEDIA_URL = "/media/"
-MEDIA_ROOT = [BASE_DIR / "media/"]
+MEDIA_ROOT = BASE_DIR / "media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

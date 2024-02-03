87aad1411f82100a544ac207a3a0fbc83e508919
meow-d <isaacwenyi@gmail.com>
2024-02-03 17:40:22 +0800

    account pages no longer in `account/` instead of `accounts/login`, it's `login/`

--- recipeapp/recipeapp/urls.py
+++ recipeapp/recipeapp/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", include("recipes.urls")),
-    path("accounts/", include("accounts.urls")),
+    path("", include("accounts.urls")),
     # path('accounts/', include('django.contrib.auth.urls')),
     path("admin/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

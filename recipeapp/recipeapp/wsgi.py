2ba971a6c1a4715aba40e9b893e0a0b611026e51
meow-d <isaacwenyi@gmail.com>
2024-02-07 12:24:32 +0800

    hacky solution that i don't undersrand

--- recipeapp/recipeapp/wsgi.py
+++ recipeapp/recipeapp/wsgi.py
@@ -8,11 +8,14 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+
+if os.getenv("POSTGRES_URL"):
+    sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "recipeapp.settings")
 
 application = get_wsgi_application()
-
 app = application

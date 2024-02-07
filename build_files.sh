db96153cc328d97667a996b63d2039014d0ab26d
meow-d <isaacwenyi@gmail.com>
2024-02-07 14:04:15 +0800

    Update collectstatic command to clear previous static files

--- build_files.sh
+++ build_files.sh
@@ -4,4 +4,4 @@ pip install -r requirements.txt
 
 # make migrations
 python3.9 recipeapp/manage.py migrate --noinput
-python3.9 recipeapp/manage.py collectstatic --noinput
+python3.9 recipeapp/manage.py collectstatic --noinput --clear

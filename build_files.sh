a40b78f52c432b09594d028683030841046954e7
meow-d <isaacwenyi@gmail.com>
2024-02-07 13:41:54 +0800

    Update migrate command in build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -3,5 +3,5 @@
 pip install -r requirements.txt
 
 # make migrations
-python3.9 recipeapp/manage.py migrate
+python3.9 recipeapp/manage.py migrate --noinput
 python3.9 recipeapp/manage.py collectstatic --noinput

97d095b384f942924e1e1468497d3c45d2c26f79
KeithLoZeHui <tp067653@mail.apu.edu.my>
2024-02-08 20:15:26 +0800

    modified admin.py code

--- recipeapp/accounts/admin.py
+++ recipeapp/accounts/admin.py
@@ -1,17 +1,38 @@
-from django.contrib import admin
-from .models import UserProfile
+from django.contrib..models import AbstractUser
+from django.db import models
 
-class UserProfile(AbstractUser):
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures', null=True, blank=True)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    age = models.PositiveIntegerField(null=True, blank=True)
+
+class CustomerUser(User):
     USER_TYPE_CHOICES = [
         ('C', 'Customer'),
+    ]
+
+    user_type = models.CharField(max_length=1, choices=USER_TYPE_CHOICES, default='C')
+
+    def __str__(self):
+        return self.username
+
+class ProfessionalUser(User):
+    USER_TYPE_CHOICES = [
         ('P', 'Professional'),
+    ]
+
+    user_type = models.CharField(max_length=1, choices=USER_TYPE_CHOICES, default='P')
+
+    def __str__(self):
+        return self.username
+
+class AdminUser(User):
+    USER_TYPE_CHOICES = [
         ('A', 'Admin'),
     ]
-    user_type = models.CharField(max_length=1, choices=USER_TYPE_CHOICES, default='C')
-    bio = models.TextField(blank=True)
-    location = models.CharField(max_length=100, blank=True)
-    website = models.URLField(blank=True)
-    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True)
+
+    user_type = models.CharField(max_length=1, choices=USER_TYPE_CHOICES, default='A')
 
     def __str__(self):
         return self.username

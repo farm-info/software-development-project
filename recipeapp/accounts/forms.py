1686e029b1b33650ec6d7b2d9217a231575aafcf
meow-d <isaacwenyi@gmail.com>
2024-02-08 21:58:02 +0800

    remove unused code

--- recipeapp/accounts/forms.py
+++ recipeapp/accounts/forms.py
@@ -6,3 +6,12 @@ class FullUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = UserCreationForm.Meta.fields + ("first_name", "last_name", "email")  # type: ignore
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update(
+                {
+                    "placeholder": f"{field.label} - Required",
+                }
+            )

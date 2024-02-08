dd99de75899330918fb9d6323a5d0ca70c287168
meow-d <isaacwenyi@gmail.com>
2024-02-08 20:41:07 +0800

    Revert keith's commits
    Revert "this is rosak"
    This reverts commit c285183bd6e38e6aeda0ff5cdd4b986c82b83b3e.
    Revert "made email primary key"
    
    This reverts commit 200a68831580b2e0c0dfbca68504ce1ae01432c3.
    
    Revert "modified admin.py code"
    
    This reverts commit 97d095b384f942924e1e1468497d3c45d2c26f79.
    
    Revert "added some things"
    
    This reverts commit 3b06d9e48afd4656d3b8b199dfbe3b08415b747b.
    
    a

--- recipeapp/accounts/admin.py
+++ recipeapp/accounts/admin.py
@@ -1,38 +1,4 @@
-from django.contrib.models import AbstractUser
-from django.db import models
+from django.contrib import admin
+from .models import UserProfile
 
-class User(AbstractUser):
-    email = models.EmailField(unique=True, primary_key=True)
-    profile_picture = models.ImageField(upload_to='profile_pictures', null=True, blank=True)
-    phone_number = models.CharField(max_length=20, null=True, blank=True)
-    age = models.PositiveIntegerField(null=True, blank=True)
-
-class CustomerUser(User):
-    USER_TYPE_CHOICES = [
-        ('C', 'Customer'),
-    ]
-
-    user_type = models.CharField(max_length=1, choices=USER_TYPE_CHOICES, default='C')
-
-    def __str__(self):
-        return self.username
-
-class ProfessionalUser(User):
-    USER_TYPE_CHOICES = [
-        ('P', 'Professional'),
-    ]
-
-    user_type = models.CharField(max_length=1, choices=USER_TYPE_CHOICES, default='P')
-
-    def __str__(self):
-        return self.username
-
-class AdminUser(User):
-    USER_TYPE_CHOICES = [
-        ('A', 'Admin'),
-    ]
-
-    user_type = models.CharField(max_length=1, choices=USER_TYPE_CHOICES, default='A')
-
-    def __str__(self):
-        return self.username
+admin.site.register(UserProfile)

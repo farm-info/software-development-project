411875b3c9a83eeb84f75e491cf0271a6896287c
meow-d <isaacwenyi@gmail.com>
2024-02-08 22:34:38 +0800

    make email unique, and add help message

--- recipeapp/accounts/models.py
+++ recipeapp/accounts/models.py
@@ -3,6 +3,11 @@ from django.db import models
 
 
 class User(AbstractUser):
+    email = models.EmailField(
+        unique=True,
+        blank=True,
+        help_text="Optional but recommended. Used for account recovery.",
+    )
     bio = models.TextField(blank=True)
     profile_picture = models.ImageField(
         upload_to="media/profile_pictures/", null=True, blank=True

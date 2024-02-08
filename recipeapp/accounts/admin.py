3b06d9e48afd4656d3b8b199dfbe3b08415b747b
KeithLoZeHui <tp067653@mail.apu.edu.my>
2024-02-08 20:05:23 +0800

    added some things

--- recipeapp/accounts/admin.py
+++ recipeapp/accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+class UserProfile(AbstractUser):
+    USER_TYPE_CHOICES = [
+        ('C', 'Customer'),
+        ('P', 'Professional'),
+        ('A', 'Admin'),
+    ]
+    user_type = models.CharField(max_length=1, choices=USER_TYPE_CHOICES, default='C')
+    bio = models.TextField(blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    website = models.URLField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True)
+
+    def __str__(self):
+        return self.username

b298c62efc3e456fd704ce1671f562e06a180bfa
meow-d <isaacwenyi@gmail.com>
2024-02-08 22:18:20 +0800

    remove command to flush

--- build_files.sh
+++ build_files.sh
@@ -3,6 +3,5 @@
 pip install -r requirements.txt
 
 # make migrations
-python3.9 manage.py flush
 python3.9 recipeapp/manage.py migrate --noinput
 python3.9 recipeapp/manage.py collectstatic --noinput --clear

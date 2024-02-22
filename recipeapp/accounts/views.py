49d6b6b0f62c2915bef4e350e1b78b87642a9185
KeithLoZeHui <tp067653@mail.apu.edu.my>
2024-02-22 20:09:44 +0800

    change/ added functions

--- recipeapp/accounts/views.py
+++ recipeapp/accounts/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
 from .forms import AdminProfileForm, FullUserCreationForm
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 
 
 from django.contrib.auth.forms import UserChangeForm

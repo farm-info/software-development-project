04ec4bca96e1671a6751fd85587bbc426f06100d
meow-d <isaacwenyi@gmail.com>
2024-03-06 19:45:47 +0800

    app config to only initialize loader when ready

--- recipeapp/recipes/apps.py
+++ recipeapp/recipes/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+from .tfidf_loader import TfidfLoaderSingleton
 
 
 class RecipesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'recipes'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "recipes"
+
+    def ready(self):
+        loader = TfidfLoaderSingleton.get_instance()
+        loader.initialize()
+        print("TfidfLoader initialized!")

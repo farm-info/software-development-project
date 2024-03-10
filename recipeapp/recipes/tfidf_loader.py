30db76f96578697eee9b571aa46e40fa64bb644a
meow-d <isaacwenyi@gmail.com>
2024-03-10 15:07:42 +0800

    Remove comment

--- recipeapp/recipes/tfidf_loader.py
+++ recipeapp/recipes/tfidf_loader.py
@@ -6,7 +6,7 @@ import joblib
 
 
 class TfidfLoader:
-    # WONTFIX it doesn't ensure that it's initialized, it sucks. i'm sorry.
+    # WONTFIX it sucks. i'm sorry.
     # use `type: ignore` to suppress the errors
     def __init__(self):
         if os.path.exists("tfidf_vectorizer.joblib"):

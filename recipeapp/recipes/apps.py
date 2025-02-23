956c5b70cbe0f9c5b43fd4ab8abf34e2c597f2a3
meow-d <meow_d@tuta.io>
2025-02-23 16:13:28 +0800

    fix: don't init tfidf when running migrate command

--- recipeapp/recipes/apps.py
+++ recipeapp/recipes/apps.py
@@ -1,3 +1,4 @@
+import sys
 from django.apps import AppConfig, apps
 from .tfidf_loader import TfidfLoaderSingleton
 
@@ -6,8 +7,16 @@ class RecipesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "recipes"
 
+    def initialize_tfidf_loader(self):
+        try:
+            recipe_model = apps.get_model("recipes", "Recipe")
+            loader = TfidfLoaderSingleton.get_instance()
+            loader.initialize(recipe_model)
+            print("TfidfLoader initialized!")
+        except Exception as e:
+            print(f"Failed to initialize TfidfLoader: {e}")
+
     def ready(self):
-        loader = TfidfLoaderSingleton.get_instance()
-        recipe_model = apps.get_model("recipes", "Recipe")
-        loader.initialize(recipe_model)
-        print("TfidfLoader initialized!")
+        # TODO: kinda janky
+        if "migrate" not in sys.argv:
+            self.initialize_tfidf_loader()
